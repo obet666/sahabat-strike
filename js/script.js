986aff902ef69ff7ec93ae6f0d2d9c27eaf261a4
obet666 <rmsxx99@gmail.com>
2024-04-01 19:07:46 +0700

    merubah ukuran logo navbar

--- js/script.js
+++ js/script.js
@@ -1,9 +1,34 @@
-//Toggle Class Active
+//Toggle Class Active Untuk Humberger Menu
 
-const navbarNav = document.querySelector('.navbar-nav');
+const navbarNav = document.querySelector(".navbar-nav");
 
 // Ketika Hanburger Menu Diklik
 
-document.querySelector('#hamburger-menu').onclick = () => {
-  navbarNav.classList.toggle('active');
+document.querySelector("#hamburger-menu").onclick = (e) => {
+  navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
+
+// Toggle Class Active Untuk Search Form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-btn").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+//klik diluar element
+
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-btn");
+
+document.addEventListener("click", function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+});
